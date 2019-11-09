be65328fa07bd52f5fd746aab1a28c72a65b5fad
Andriy <asoha474@gmail.com>
2019-11-09 21:46:14 +0200

    add abstract class 'ElectricalAppliance'

---
+++ src/main/java/com/epam/model/ElectricalAppliance.java
@@ -0,0 +1,47 @@
+package com.epam.model;
+
+public abstract class ElectricalAppliance {
+
+    private String name;
+    private int power;
+    private boolean isConnected;
+
+    public ElectricalAppliance(String name, int power, boolean isConnected) {
+        this.name = name;
+        this.power = power;
+        this.isConnected = isConnected;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPower() {
+        return power;
+    }
+
+    public void setPower(int power) {
+        this.power = power;
+    }
+
+    public boolean isConnected() {
+        return isConnected;
+    }
+
+    public void setConnected(boolean connected) {
+        isConnected = connected;
+    }
+
+    @Override
+    public String toString() {
+        return "ElectricalAppliance{" +
+                "name='" + name + '\'' +
+                ", power=" + power +
+                ", isConnected=" + isConnected +
+                '}';
+    }
+}
